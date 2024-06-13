57c5a2c969398d44c420efe30def344f64237eae
wnsadmi <soaadmn@gmail.com>
2024-06-13 20:34:46 +0530

    proj

---
+++ src/main/java/grp/one/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package grp.one;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author krieger
+ */
+@ApplicationPath("rest")
+public class JAXRSConfiguration extends Application {
+    
+}
